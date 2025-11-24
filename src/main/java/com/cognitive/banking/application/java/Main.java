e4c59c254d9d8898cc4cf15229c509a066734e73
Matanga-F <matangafulufhelo@gmail.com>
2025-11-24 20:14:29 +0200

    I've added a minimum structure for the banking application to to build, test & deploy

---
+++ src/main/java/com/cognitive/banking/application/java/Main.java
@@ -0,0 +1,9 @@
+package com.cognitive.banking.application.java;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
